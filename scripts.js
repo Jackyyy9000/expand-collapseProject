99be2edc9f87e6e538363be613aa03805372ed6f
Jackyyy9000 <141958364+Jackyyy9000@users.noreply.github.com>
2023-08-11 17:51:51 +0200

    Add logic to collapse and expand
    It is not the nicest solution, however, I was focusing on navigating through the DOM rather than going fancy.

--- scripts.js
+++ scripts.js
@@ -2,12 +2,16 @@ const cards = document.querySelectorAll(".card");
 
 cards.forEach((element) =>
   element.addEventListener("click", (e) => {
+    let cardBody = e.target.parentElement.nextElementSibling;
+
     if (!e.target.matches(".expand-button")) return;
 
-    if (e.target.innerText === "Expand") {
-      e.target.innerText = "Collapse";
-    } else {
+    if (e.target.innerText === "Collapse") {
       e.target.innerText = "Expand";
+      cardBody.classList.remove("show");
+    } else {
+      e.target.innerText = "Collapse";
+      cardBody.classList.add("show");
     }
   })
 );
